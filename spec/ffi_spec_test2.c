eb88eac32f89afc8010df737905cb2e075bb3c15
Kyra 'nonchip' Zimmer <me@nonchip.de>
2015-08-04 10:52:09 +0200

    self compiling

--- spec/ffi_spec_test2.c
+++ spec/ffi_spec_test2.c
@@ -1,29 +1,45 @@
 #if 0
 cd "$(dirname "$(readlink -f "$0")")"
-gcc -shared -Wall -fPIC "$(basename "$0")" -o "$(basename "$0" .c).so"
+
+awk '/^\-\-\-\-/ {ofn="/tmp/split_" $2 ".bin"} ofn {print > ofn}' "$(basename "$0")"
+
+echo "const char CDEF_$(basename "$0" .c)_A[]={" > /tmp/defs.c
+tail -n +2 /tmp/split_CDEF.bin | xxd -i >> /tmp/defs.c
+echo ",0}; const char LDEF_$(basename "$0" .c)_A[]={" >> /tmp/defs.c
+tail -n +2 /tmp/split_LDEF.bin | xxd -i >> /tmp/defs.c
+echo ",0}; const char MDEF_$(basename "$0" .c)_A[]={" >> /tmp/defs.c
+tail -n +2 /tmp/split_MDEF.bin | xxd -i >> /tmp/defs.c
+echo ",0};" >> /tmp/defs.c
+cat >>/tmp/defs.c <<END
+const char *CDEF_$(basename "$0" .c)=&CDEF_$(basename "$0" .c)_A[0];
+const char *LDEF_$(basename "$0" .c)=&LDEF_$(basename "$0" .c)_A[0];
+const char *MDEF_$(basename "$0" .c)=&MDEF_$(basename "$0" .c)_A[0];
+END
+
+gcc -shared -Wall -fPIC "$(basename "$0")" /tmp/defs.c -o "$(basename "$0" .c).so"
+rm /tmp/split_*.bin /tmp/defs.c
 exit
+
+---- CDEF
+const int a;
+
+---- LDEF
+function b(x,y)
+  return C.a+x+y
+end
+str='blah'
+return {b=b,str=str}
+
+---- MDEF
+ffi=require'ffi'
+local c
+if C.a==1
+  c=ffi.string L.str
+else
+  c='that shouldn\'t happen'
+{:c}
+---- END
+
 #endif
-
-const char* CDEF_ffi_spec_test2=
-  "const int a;"
-;
-
-const char* LDEF_ffi_spec_test2=
-  "function b(x,y)\n"
-  "  return C.a+x+y\n"
-  "end\n"
-  "str='blah'\n"
-  "return {b=b,str=str}\n"
-;
-
-const char* MDEF_ffi_spec_test2=
-  "ffi=require'ffi'\n"
-  "local c\n"
-  "if C.a==1\n"
-  "  c=ffi.string L.str\n"
-  "else\n"
-  "  c='that shouldn\\'t happen'\n"
-  "{:c}\n"
-;
 
 const int a=1;
